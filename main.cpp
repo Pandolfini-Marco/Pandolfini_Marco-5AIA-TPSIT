da37f69a2cb9d4b4ef0433b7b88ca83d8a08e8a9
Marco Pandolfini <marcojezz@alice.it>
2017-11-21 09:43:31 +0100

    main

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/*
+  Ciclo while da 1 a 10 
+*/
+#include<iostream>
+using namespace std;
+  void main()
+{
+int c;// variabile contatore
+	do{
+          cout<<c++<<endl;
+	}while(c<=10)
+   system("PAUSE");
+   return 0;
+}
